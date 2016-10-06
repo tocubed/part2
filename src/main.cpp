1a5cb9d9e4e307035e13ae9298d96ebd362d98f6
Priyank Patel <tocubed@gmail.com>
2016-10-06 12:55:17 -0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,62 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+
+sf::RenderWindow* window;
+bool quit;
+
+void update(sf::Time delta) {
+
+	sf::Event event;
+	while(window->pollEvent(event)) {
+		if(event.type == sf::Event::Closed)
+			quit = true;
+	}
+
+}
+
+void render(sf::Time delta) {
+
+	window->clear(sf::Color::Black);
+	window->display();
+
+}
+
+void loop() {
+
+	sf::Clock clock;
+	sf::Time accumulator;
+
+	const auto max_frame_time = sf::milliseconds(100);
+	const auto update_time_step = sf::milliseconds(30);
+
+	while(!quit) {
+		auto elapsed = clock.restart();
+
+		if(elapsed > max_frame_time)
+			elapsed = max_frame_time;
+
+		accumulator += elapsed;
+		while(accumulator >= update_time_step) {
+			update(update_time_step);
+
+			accumulator -= update_time_step;
+		}
+
+		render(accumulator);
+	}
+
+}
+
+int main(int argc, char** argv) {
+
+	sf::RenderWindow window_(sf::VideoMode(800, 600), "Test");
+	window = &window_;
+
+	quit = false;
+
+	loop();
+	window_.close();
+
+	return EXIT_SUCCESS;
+
+}
